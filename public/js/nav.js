de19c83d5a0ee3e87966c0ae669815ebc107e385
Mithlesh kumar <mithleshk000@gmail.com>
2024-05-29 00:30:59 +0530

    In(signup login)page signin.css input border-bottom on hover fixed.\n In boloPage.ejs nav.ejs toggel userprofile is fixed

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,4 +1,15 @@
-const subMenu = document.getElementById("subMenu");
-function toggelMenu() {
-    subMenu.classList.toggle("open-menu");
-}
+// onclick="toggelMenu() not in use"
+// const subMenu = document.getElementById("subMenu");
+// function toggelMenu() {
+//     subMenu.classList.toggle("open-menu");    
+// }
+window.onclick = function(event) {
+    var userImage = document.getElementById("userImage");
+    var subMenu = document.getElementById("subMenu");
+
+    if(event.target === userImage){
+        subMenu.classList.toggle("open-menu"); // Toggle class to open/close menu
+    } else {
+        subMenu.classList.remove("open-menu"); // Close menu if clicked outside userImage
+    }
+};
