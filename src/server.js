384aa28ebafa3f80cfb37782079d8ab0cbf80b38
Mithlesh kumar <mithleshk000@gmail.com>
2024-06-05 14:41:14 +0530

    views file added to src file structure

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const port = process.env.PORT || 3000;
 // const filePath=path;
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, 'views'));
 
 
 // Set up other middleware and routes...
